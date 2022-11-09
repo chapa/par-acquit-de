518aa530ea50078871b131e1d2a62801570799fc
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2022-11-09 21:08:21 +0100

    feature: add templating and home design

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,27 @@ mod error;
 
 use crate::data::Data;
 use rocket::{Build, Rocket, State};
+use rocket::fs::FileServer;
+use rocket_dyn_templates::{context, Template};
 
 #[get("/")]
-fn index(data: &State<Data>) -> String {
-    format!("{:#?}", data.get_random_word())
+fn index(data: &State<Data>) -> Template {
+    let word = data.get_random_word().unwrap();
+
+    Template::render("index", context! {
+        value: word.get_value(),
+        quote: word.get_quote(),
+        keywords: word.get_keywords(),
+        title: format!("Par acquit de {}", word.get_value()),
+        contentClasses: "top"
+    })
 }
 
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
         .manage(Data::from_path("data.csv"))
+        .mount("/", FileServer::from("public/"))
         .mount("/", routes![index])
+        .attach(Template::fairing())
 }
