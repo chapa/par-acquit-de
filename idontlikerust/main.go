00e563ac88ee23a3080a49aa6a49da2b090c663e
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2023-03-10 00:06:20 +0100

    dockerfile: fix doc

--- idontlikerust/main.go
+++ idontlikerust/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"net/http"
+	"time"
 )
+
+var csv = bytes.NewBuffer(nil)
+var lastFetch = time.Time{}
 
 func main() {
 
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("https://framadate.org/exportcsv.php?poll=4eF5QE9cHUch9HF1")
-		if err != nil {
-			panic(err)
+
+		r.Header.Set("Access-Control-Allow-Origin", "*")
+		r.Header.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		r.Header.Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if time.Since(lastFetch) > 1*time.Minute {
+			resp, err := http.Get("https://framadate.org/exportcsv.php?poll=4eF5QE9cHUch9HF1")
+			if err != nil {
+				panic(err)
+			}
+
+			defer resp.Body.Close()
+
+			csv = bytes.NewBuffer(nil)
+			_, err = io.Copy(csv, resp.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			lastFetch = time.Now()
 		}
 
-		defer resp.Body.Close()
+		w.Write(csv.Bytes())
 
-		_, err = io.Copy(w, resp.Body)
-		if err != nil {
-			panic(err)
-		}
 	}))
 
 	http.ListenAndServe(":8080", nil)
